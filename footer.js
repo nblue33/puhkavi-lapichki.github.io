4e869f0194003b147bdc5b6950bda2ee6e27e123
nblue33 <124361@students.ue-varna.bg>
2025-05-31 17:37:05 +0300

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,17 @@
+var x = document.createElement('footer');
+footer.className= 'footer';
+
+footer.innerHTML=`
+  <p class="a">Социални мрежи :</p>
+  <a href="https://www.facebook.com/profile.php?id=61575952360171">Facebook</a>
+  <a href="https://www.instagram.com/puhkavi_lapichki_varna/">Instagram</a> 
+
+  <p class="b"></p>
+  <a href="file:///C:/Users/nutfi/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%D0%BD%20%D0%BF%D0%BB%D0%BE%D1%82/Visual%20Code/pets.html">Oсиновяване</a>
+
+  <p class="c"></p>
+  <a href="file:///C:/Users/nutfi/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%D0%BD%20%D0%BF%D0%BB%D0%BE%D1%82/Visual%20Code/contacts.html">Контакти</a>
+
+  <p class="d">&copy;2025 Пухкави лапички. Сайтът е с учебна цел.</p>
+`;
+document.body.appendChild(footer);
